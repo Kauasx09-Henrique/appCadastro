5fc04ea164abb467cea190d2ce4cd1afcb484e6c
Kaua Henrique <kauahenriquesx09@gmail.com>
2025-04-25 21:32:17 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons'; // Para ícones
 import CadastroScreen from './Telas/CadastroScreens.jsx';
 import { ScrollView } from 'react-native-gesture-handler';
 import LoginScreen from './Telas/LoginScreens.jsx';
+import Perfil from './Telas/PerfilScreens.jsx';
 
 const Tab = createBottomTabNavigator();
 
@@ -22,8 +23,12 @@ export default function App() {
             tabBarIcon: () => <Ionicons name="Home" size={24} />
           }} 
         />
-                <Stack.Screen name="Login" component={LoginScreen} options={{
+                <Tab.Screen name="Login" component={LoginScreen} options={{
                   tabBarIcon: () => <Ionicons name='' size={24}/>
+                }}
+                />
+                <Tab.Screen name="Perfil" component={Perfil} options={{
+                  tabBarIcon: () => <Ionicons name='options' size={24}/>
                 }}
                 />
       </Tab.Navigator>
